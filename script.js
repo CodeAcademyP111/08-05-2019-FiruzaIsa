7ed3d3b21964254379e0ea897eae4c8ed8760caf
FiruzaIsa <49427451+FiruzaIsa@users.noreply.github.com>
2019-05-10 13:43:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// bu gunki tapwirirq
+// iki tam ededi ucuncu deyiwen olmadan yerlerini deyiwin
+
+
+
+var a = Number(prompt("a-ni daxil et"));
+var b = Number(prompt("b-ni daxil et"));
+
+
+var a = 5
+var b = 8
+a = a*b;
+b = a/b;
+a = a/b; 
+console.log(a)
+console.log(b)
