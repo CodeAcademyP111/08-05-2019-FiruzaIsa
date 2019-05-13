aec881e9781b67424e61074625080860ee54f367
FiruzaIsa <49427451+FiruzaIsa@users.noreply.github.com>
2019-05-13 10:30:54 +0400

    Update duplicate.js

--- duplicate.js
+++ duplicate.js
@@ -2,7 +2,7 @@ var array = [17,15,14,17,21,14];
 var findDupl_arr = [];
 for (var i=0; i<array.length; i++){
     for (var j=i+1;j<array.length; j++){
-        if(array[i]==array[j]){
+        if(array[i]==array[j] && !findDupl_arr.includes(array[i])){
             findDupl_arr.push(array[i]);
         }
     }
